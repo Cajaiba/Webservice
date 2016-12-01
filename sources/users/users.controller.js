1ef34407aa2dd1c5f7be961dc3a093573cfd8116
Eduardo <cajaibaeduardo@gmail.com>
2016-11-30 22:48:34 -0200

    add create method to users.controllers and remove logs from that

--- sources/users/users.controller.js
+++ sources/users/users.controller.js
@@ -2,10 +2,36 @@ import Users from './users.model.js'
 
 module.exports = {
   list,
+  get,
+  disable,
+  create,
 }
 
 function list(req, res) {
-  Users.find({}, function(err, users) {
-    res.json(users)  
-  })
+  Users
+    .find({active: {$ne: false}})
+    .then(users => res.json(users))
 }
+
+function get(req, res) {
+  Users
+    .findById(req.params.id)
+    .then(users => res.json(users))
+} 
+
+function create(req, res) {
+  const user = Users(req.body)
+
+  user
+    .save()
+    .then(() => res.status(201).json({message: 'create'}))
+    .catch((err) => {
+      res.status(400).json({message: err.message})
+    })
+}
+
+function disable(req, res) {
+  Users
+    .findByIdAndUpdate(req.params.id, {$set: {active: false}})
+    .then(() => res.json({message: 'disable'}))
+}   
